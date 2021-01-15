3f3778db54922957184b123408e18b4abb57be7d
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-14 23:15:42 -0500

    se termina el ejercicio 3

--- js/13-ejercicio3.js
+++ js/13-ejercicio3.js
@@ -1,3 +1,12 @@
 'use strict'
 
+/*
+hacer un programa que muestre todos los numeros 
+entre 2 numeros introducidos por el usuario
+*/
 
+var numero2 = prompt('introduce el segundo numero');
+
+for (var numero1 = prompt('introduce el primer numero'); numero1 <= numero2; numero1++) {
+    console.log(numero1);
+}
