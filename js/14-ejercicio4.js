8b225588e7e1b814f56429b11626fcf476f32c24
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-18 00:44:43 -0500

    se agrega un ejercicio nuevo

--- js/14-ejercicio4.js
+++ js/14-ejercicio4.js
@@ -1,3 +1,16 @@
 'use strict'
+/*
+mostrar todos los numeros impares que hay
+entre 2 numeros introducidos por el usuario
+*/
 
+var numero1 = prompt('introduce el numero inicial')
+var numero2 = prompt('introduce el numero final');
 
+while (numero1 < numero2) {
+    numero1++;
+
+    if (numero1 % 2 != 0) {
+        console.log('el numero ' + numero1 + ' es impar');
+    }
+}
