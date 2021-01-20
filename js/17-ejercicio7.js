4e0aade167f26a035b85cc44fac61c35604b0dac
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-20 09:19:19 -0500

    se resuelve el ejercicio 7

--- js/17-ejercicio7.js
+++ js/17-ejercicio7.js
@@ -1,1 +1,24 @@
 'use strict'
+
+/*
+Tabla de multiplicar de un numero
+introducido en pantalla.
+*/
+
+var numero = prompt('introduce un numero');
+
+document.write('<h1>tabla del '+ numero +'</h1>');
+
+for (let i = 0; i <= 10; i++) {
+    var resultado = numero * i;
+    document.write(numero + ' x ' + i + ' = ' + resultado + '<br>');
+}
+
+// Todas las tablas de mult.
+for (let c = 0; c <= 10; c++) {
+    document.write('<h1>tabla del '+ c +'</h1>');
+    for (let i = 0; i <= 10; i++) {
+        var resultado = c * i;
+        document.write(c + ' x ' + i + ' = ' + resultado + '<br>');
+    }
+}
