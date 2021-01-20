10baf1d148f3ae7052d011e751d31408cad7692b
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-20 08:40:25 -0500

    se termina el ejercicio

--- js/16-ejercicio6.js
+++ js/16-ejercicio6.js
@@ -1,3 +1,19 @@
 'use strict'
 
+/*
+que nos diga si es par o impar
+1. ventana que pida el numero
+2. si el numero no es valido que lo pida de nuevo
+*/
 
+var numero = parseInt(prompt('introduce un numero'));
+
+while (isNaN(numero)) {
+    numero = parseInt(prompt('introduce un numero'));
+}
+
+if (numero % 2 == 0) {
+    alert('es un numero par');
+}else{
+    alert('es impar');
+}
