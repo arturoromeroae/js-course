e73e38c04c5ee7777c1a3f693958f4a3d5917693
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-21 16:39:19 -0500

    se argrega funcion

---
+++ js/19-funciones.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/*
+    funciones
+    es una agrupacion reutilizable de un conjunto
+    de instrucciones que se ultilizan las veces que
+    deseamos.
+*/
+
+function calculadora() {
+    return "hola soy la calculadora";
+}
+
+console.log(calculadora);
