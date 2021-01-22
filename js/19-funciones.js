bf211e828644f7e2d8d9206671d13f36a6919178
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-22 15:20:34 -0500

    se agrega archivo parametros

--- js/19-funciones.js
+++ js/19-funciones.js
@@ -7,16 +7,36 @@
     deseamos.
 */
 // defino la funcion
-function calculadora(numero1, numero2) {
-    // conjunto de instrucciones a ejecutar
+function porConsola(numero1, numero2) {
     console.log("suma: " + (numero1 + numero2));
     console.log("resta: " + (numero1 - numero2));
     console.log("multiplicacion: " + (numero1 * numero2));
     console.log("division: " + (numero1 / numero2));
+}
 
-    // return "hola soy la calculadora";
+function porPantalla(numero1, numero2) {
+    document.write("suma: " + (numero1 + numero2) + '<br>');
+    document.write("resta: " + (numero1 - numero2) + '<br>');
+    document.write("multiplicacion: " + (numero1 * numero2) + '<br>');
+    document.write("division: " + (numero1 / numero2) + '<br>');
+}
+
+function calculadora(numero1, numero2, mostrar = false) {
+
+    // conjunto de instrucciones a ejecutar
+
+    if (mostrar == false) {
+        porConsola(numero1, numero2);
+    }else{
+        porPantalla(numero1, numero2);
+    }
+
+    return true;
 }
 // invocar o llamar a la funcion
+calculadora(2, 5);
+calculadora(1, 4, true);
+
 /*
 for (let i = 1; i < 10; i++) {
     console.log(i);
