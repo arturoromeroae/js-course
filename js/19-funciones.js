2718d5125ca4586126dd6f952ef535892b66f9f6
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-21 21:23:42 -0500

    se argrega funcion

--- js/19-funciones.js
+++ js/19-funciones.js
@@ -7,12 +7,19 @@
     deseamos.
 */
 // defino la funcion
-function calculadora() {
+function calculadora(numero1, numero2) {
     // conjunto de instrucciones a ejecutar
-    console.log("hola soy la calculadora");
-    console.log("si soy yo");
+    console.log("suma: " + (numero1 + numero2));
+    console.log("resta: " + (numero1 - numero2));
+    console.log("multiplicacion: " + (numero1 * numero2));
+    console.log("division: " + (numero1 / numero2));
 
     // return "hola soy la calculadora";
 }
 // invocar o llamar a la funcion
-calculadora()
+/*
+for (let i = 1; i < 10; i++) {
+    console.log(i);
+    calculadora(i, 8);
+}
+*/
