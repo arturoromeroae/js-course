a7cd54d56f859f11adacf50193c5d492890c5fd3
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-23 22:34:38 -0500

    se agrega nuevo archivo de funciones

--- js/22-ambito.js
+++ js/22-ambito.js
@@ -1,8 +1,11 @@
 'use strict'
 
 function holaMundo(texto) {
+    var hola_mundo = "texto dentro de funcion";
     console.log(texto);
-    console.log()
+    // convertir un objeto a string
+    console.log(numero.toString());
+    console.log(hola_mundo);
 }
 
 var numero = 12;
