b671678ef23dddcb9c2f8c6331c59b8c3a019848
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-23 23:02:42 -0500

    se agregan funciones

--- js/23-funciones-texto.js
+++ js/23-funciones-texto.js
@@ -1,3 +1,21 @@
 'use strict'
 
+// transformacion de textos
+var numero = 565;
+var texto1 = "bienvenidos al curso de js";
+var texto2 = "es muy buen curso";
 
+// var dato = numero.toString();
+// dato = texto1.toUpperCase();
+// dato = texto2.toLowerCase();
+// console.log(dato);
+
+// // calcular longitud
+// var nombre = "arturo romero";
+//     nombre = ["hoa", "hola"];
+// console.log(nombre.length);
+
+// // concatenar - unir textos
+// var textoTotal = texto1 + " " + texto2;
+// textoTotal = texto1.concat(" " + texto2);
+// console.log(textoTotal);
