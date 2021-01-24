bab9d5afb02a86dbef78da90d34cd7b1905250ae
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-24 13:08:55 -0500

    se agrega fichero plantillas

--- js/23-funciones-texto.js
+++ js/23-funciones-texto.js
@@ -19,3 +19,29 @@ var texto2 = "es muy buen curso";
 // var textoTotal = texto1 + " " + texto2;
 // textoTotal = texto1.concat(" " + texto2);
 // console.log(textoTotal);
+ var busqueda = texto1.indexOf("curso");
+ busqueda = texto1.lastIndexOf("curso");
+ busqueda = texto1.search("curso");
+ // solo busca la primera coincidencia
+ busqueda = texto1.match("curso");
+ // busca todas las coincidencias
+ busqueda = texto1.match(/curso/g);
+// sebstrae texto desde y hasta los num indicados
+ busqueda = texto1.substr(14, 5);
+// substrae la letra indicada
+ busqueda = texto1.charAt(10);
+// busca si existe al inicio (true o false)
+ busqueda = texto1.startsWith("bi");
+ busqueda = texto1.endsWith("js");
+// busca una palabra (true o false)
+busqueda = texto1.includes("juan");
+// remplazar texto
+busqueda = texto1.replace("curso", "estudio");
+// separa o corta un string
+busqueda = texto1.slice(14, 22);
+// recorta palabras y meter en un array
+busqueda = texto1.split(' ');
+// quita espacios en un string
+busqueda = texto1.trim();
+
+console.log(busqueda);
