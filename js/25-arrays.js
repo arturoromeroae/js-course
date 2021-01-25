88244063e502f2b10610931de3a4e8f4a28f9686
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-24 22:51:39 -0500

    se agrega fichero de ejercicios

--- js/25-arrays.js
+++ js/25-arrays.js
@@ -21,14 +21,29 @@ if (elemento >= nombres.length) {
 
 document.write("<h1>lenguajes del 2021</h1>");
 document.write("<ul>");
+// *********** Iteraciones y recorrer arrays 1 ***********
 // for (let i = 0; i < lenguajes.length; i++) {
 //     document.write("<li>" + lenguajes[i] + "</li>");
     
 // }
+// *********** Iteraciones y recorrer arrays 2 ***********
+// lenguajes.forEach((elemento, index, array)=>{
+//     console.log(array);
+//     document.write("<li>" + index + ' - ' + elemento + "</li>");
+// })
+// *********** Iteraciones y recorrer arrays 3 ***********
+for (let lenguaje in lenguajes) {
+    document.write("<li>" + lenguajes[lenguaje] + "</li>");
+}
 
-lenguajes.forEach((elemento, index, array)=>{
-    console.log(array);
-    document.write("<li>" + index + ' - ' + elemento + "</li>");
-})
+var precios = [10, 20, 30, 50, 60];
+// busquedas en arrays
+var busqueda = lenguajes.find(lenguaje => lenguaje == "php");
+// buscar indice de un elemento en el array
+var busqueda = lenguajes.findIndex(lenguaje => lenguaje == "php");
+// flitro si existen valores >, <, ==, entre otros
+var busqueda = precios.some(precio => precio < 10);
+
+console.log(busqueda);
 
 document.write("</ul>");
--- js/26-arrays-multi.js
+++ js/26-arrays-multi.js
@@ -1,3 +1,41 @@
 'use strict'
 
+var categorias = ['accion', 'terror', 'comedia'];
+var peliculas = ['batman', 'conjuro', 'si señor'];
 
+var cine = [categorias, peliculas];
+
+// console.log(cine[0][1]);
+// console.log(cine[1][2]);
+
+/*
+var elemento = '';
+
+do{
+    elemento = prompt('agrega una pelicula');
+    // agrega elemento a array
+    peliculas.push(elemento);
+}while (elemento != 'acabar');
+// elimina el ultimo elemento de un array
+peliculas.pop();
+*/
+
+var indice = peliculas.indexOf('conjuro');
+
+if (indice > -1) {
+    // elimina el elemento en la posicion indicada
+    peliculas.splice(indice, 1);
+}
+
+// convertir array a texto
+var texto = peliculas.join();
+
+//convertir candena de texto en arrays
+var cadena = "texto1, texto2, texto3";
+var cadena_array = cadena.split(", ")
+
+// odernar arrays alfabeticamente
+peliculas.sort();
+// ordena los arrays a la inversa
+peliculas.reverse();
+console.log(peliculas);
