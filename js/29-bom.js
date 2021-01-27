29ed8857c40e6508822dd2f9a0262f5e07975fc2
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-26 23:13:01 -0500

    se agrega fichero eventos

--- js/29-bom.js
+++ js/29-bom.js
@@ -1,2 +1,25 @@
 'use strict'
 
+// BOM Browser Object Model
+
+// tamaño de la ventana del navegador
+function getBoom() {
+    console.log(window.innerWidth);
+    console.log(window.innerHeight);
+    // ver la url actual del navegador
+    console.log(window.location);
+    console.log(window.location.href);
+}
+
+function redirect(url) {
+    window.location.href = url;
+}
+
+function abrirVentana(url) {
+    window.open(url, "", "width = 400 height = 300");
+}
+
+getBoom();
+// tamaño de pantalla
+console.log(screen.width);
+console.log(screen.height);
