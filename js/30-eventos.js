3163490d6902a1478e50748c4e95483b9bddfd28
Arturo Romero <aromero@aeonlinesolutions.com>
2021-01-26 23:59:02 -0500

    se agrega fichero eventos

--- js/30-eventos.js
+++ js/30-eventos.js
@@ -1,3 +1,35 @@
 'use strict'
 
+// eventos del raton
+
+function cambiarColor(params) {
+    var bg = boton.style.background;
+    if (bg == "green") {
+        boton.style.background = "red";
+    }else{
+        boton.style.background = "green";
+    }
+
+    boton.style.padding = "10px";
+    boton.style.border = "1px solid #ccc";
+
+    return true;
+}
+
+var boton = document.querySelector("#boton");
+
+// click
+boton.addEventListener('click', function () {
+    cambiarColor();
+});
+
+// mouse over
+boton.addEventListener('mouseover', function() {
+    boton.style.background = "yellow";
+})
+
+// mouse out
+boton.addEventListener('mouseout', function() {
+    boton.style.background = "#ccc";
+})
 
