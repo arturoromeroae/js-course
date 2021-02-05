10d81bf28a43f40a99ffc25fb7dbb1438e106478
Arturo Romero <aromero@aeonlinesolutions.com>
2021-02-04 22:52:20 -0500

    se agrega fichero de fechas

---
+++ js/37-try-catch.js
@@ -0,0 +1,9 @@
+
+try {
+    var year = 2021;
+    alert(year);
+    console.log(decodeURIComponent("https://&%asagle."));
+
+} catch (error) {
+    console.log('ha ocurrido el siguiente error -> ' + error);
+}
